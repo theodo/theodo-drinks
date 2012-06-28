88145705166bdb3f9b8348575b195e588c567f3f
Benjamin Grandfond <benjaming@theodo.fr>
2012-06-28 19:36:10 +0200

    Add mongoDB doctrine ODM

--- src/app.php
+++ src/app.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Silex\Provider\TwigServiceProvider;
+use Knp\Silex\ServiceProvider\DoctrineMongoDBServiceProvider;
 
 $app = new Silex\Application();
 
@@ -10,12 +11,22 @@ $app['debug'] = $_SERVER['REMOTE_ADDR'] === '127.0.0.1' || $_SERVER['REMOTE_ADDR
 
 
 $app->register(new TwigServiceProvider(), array(
-     'twig.path' => __DIR__ . '/views',
+    'twig.path' => __DIR__ . '/views',
 ));
 
+$app->register(new DoctrineMongoDBServiceProvider(), array(
+    'doctrine.odm.mongodb.connection_options' => array(
+        'database' => 'theodo-drinks',
+        'host' => 'localhost',
+    ),
+    'doctrine.odm.mongodb.documents' => array(
 
-$app->get('/', function() use ($app) {
-  return $app['twig']->render('index.html.twig');
+    )
+));
+
+$app->get('/', function() use ($app)
+{
+    return $app['twig']->render('index.html.twig');
 });
 
 return $app;
