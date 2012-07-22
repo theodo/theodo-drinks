9ee308f39b9a9c0d7c3b015d8e9a4eaefabbdfa3
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2012-07-22 17:31:47 +0200

    Add remember me checkbox.

--- src/Drinks/Application.php
+++ src/Drinks/Application.php
@@ -6,6 +6,7 @@ use Silex\Application as BaseApplication;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\FormServiceProvider;
 use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\SessionServiceProvider;
 use Silex\Provider\SecurityServiceProvider;
 use Silex\Provider\ValidatorServiceProvider;
 use Silex\Provider\TranslationServiceProvider;
@@ -15,6 +16,8 @@ use Knp\Silex\ServiceProvider\DoctrineMongoDBServiceProvider;
 use Drinks\Factory\TransactionFactory;
 use Drinks\Factory\RestockingFactory;
 use Drinks\Security\Provider\UserProvider;
+
+use Symfony\Component\Security\Http\Firewall\RememberMeListener;
 
 // Controller providers usage.
 use Drinks\Provider\UserControllerProvider;
@@ -76,6 +79,7 @@ class Application extends BaseApplication
         }));
 
         $this->register(new UrlGeneratorServiceProvider());
+        $this->register(new SessionServiceProvider());
         $this->register(new FormServiceProvider());
         $this->register(new ValidatorServiceProvider());
         $this->register(new TranslationServiceProvider(), array('locale_fallback' => 'fr'));
@@ -101,6 +105,40 @@ class Application extends BaseApplication
         $app = $this;
 
         $this->register(new SecurityServiceProvider());
+
+        $this['security.authentication_listener.remember_me._proto'] = $app->protect(function ($name, $options) use ($app) {
+            return $app->share(function () use ($app, $options, $name) {
+                if (isset($options['token_provider'])) {
+                    $servicesClass = 'Symfony\\Component\\Security\\Http\\RememberMe\\PersistentTokenBasedRememberMeServices;';
+                    unset($options['token_provider']);
+                } else {
+                    $servicesClass = 'Symfony\\Component\\Security\\Http\\RememberMe\\TokenBasedRememberMeServices';
+                }
+
+                $app['security.authentication_listener.remember_me.services'] = $app->share(function () use ($app, $servicesClass, $name, $options) {
+                    $key = $options['key'];
+                    unset($options['key']);
+
+                    return new $servicesClass(
+                        array($app['security.user_provider.'.$name]),
+                        $key,
+                        $name,
+                        $options,
+                        $app['logger']
+                    );
+                });
+
+
+                return new RememberMeListener(
+                    $app['security'],
+                    $app['security.authentication_listener.remember_me.services'],
+                    $app['security.authentication_manager'],
+                    $app['logger'],
+                    $app['dispatcher']
+                );
+            });
+        });
+
         $this['security.firewalls'] = array(
             'login' => array(
                 'pattern' => '^/user/login$',
@@ -111,6 +149,7 @@ class Application extends BaseApplication
                 'users'=> $this->share(function () use ($app) {
                     return new UserProvider($app['doctrine.odm.mongodb.dm'], 'Drinks\\Document\\User');
                 }),
+                'remember_me' => array('key' => isset($_SERVER['SECRET']) ? $_SERVER['SECRET'] : 'notsecrettoken')
             ),
         );
     }
