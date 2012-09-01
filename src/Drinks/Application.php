59828f5fbc9e303415eb4847bdffe0624fe82edd
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2012-09-01 19:38:21 +0200

    [master] Remove remember me for the moment.wq

--- src/Drinks/Application.php
+++ src/Drinks/Application.php
@@ -106,39 +106,6 @@ class Application extends BaseApplication
 
         $this->register(new SecurityServiceProvider());
 
-        $this['security.authentication_listener.remember_me._proto'] = $app->protect(function ($name, $options) use ($app) {
-            return $app->share(function () use ($app, $options, $name) {
-                if (isset($options['token_provider'])) {
-                    $servicesClass = 'Symfony\\Component\\Security\\Http\\RememberMe\\PersistentTokenBasedRememberMeServices;';
-                    unset($options['token_provider']);
-                } else {
-                    $servicesClass = 'Symfony\\Component\\Security\\Http\\RememberMe\\TokenBasedRememberMeServices';
-                }
-
-                $app['security.authentication_listener.remember_me.services'] = $app->share(function () use ($app, $servicesClass, $name, $options) {
-                    $key = $options['key'];
-                    unset($options['key']);
-
-                    return new $servicesClass(
-                        array($app['security.user_provider.'.$name]),
-                        $key,
-                        $name,
-                        $options,
-                        $app['logger']
-                    );
-                });
-
-
-                return new RememberMeListener(
-                    $app['security'],
-                    $app['security.authentication_listener.remember_me.services'],
-                    $app['security.authentication_manager'],
-                    $app['logger'],
-                    $app['dispatcher']
-                );
-            });
-        });
-
         $this['security.firewalls'] = array(
             'login' => array(
                 'pattern' => '^/user/login$',
@@ -149,7 +116,6 @@ class Application extends BaseApplication
                 'users'=> $this->share(function () use ($app) {
                     return new UserProvider($app['doctrine.odm.mongodb.dm'], 'Drinks\\Document\\User');
                 }),
-                'remember_me' => array('key' => isset($_SERVER['SECRET']) ? $_SERVER['SECRET'] : 'notsecrettoken')
             ),
         );
     }
