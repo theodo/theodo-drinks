2fc9e7d41e0bcd92331c2139951693b33a4065a3
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2012-09-16 23:12:30 +0200

    Delete users and add mongodb paramconverter

---
+++ src/Theodo/DrinksBundle/Request/ParamConverter/MongodbParamConverter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Theodo\DrinksBundle\Request\ParamConverter;
+
+use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\DoctrineParamConverter;
+
+/**
+ * MongodbParamConverter class.
+ *
+ * @author Benjamin Grandfond <benjamin.grandfond@gmail.com>
+ */
+class MongodbParamConverter extends DoctrineParamConverter
+{
+
+}
